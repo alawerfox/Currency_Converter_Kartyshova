be3a8219bde76e2c5d8aaddf3c19f680d2bf7c4f
alawerfox <alawerfox@gmail.com>
2022-03-24 16:38:07 +0300

    добавила splash screen

---
+++ app/src/main/java/ru/pdr/currencyconverterkartyshova/StartActivity.kt
@@ -0,0 +1,21 @@
+package ru.pdr.currencyconverterkartyshova
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+class StartActivity : AppCompatActivity() {
+
+    private val handler = Handler(Looper.getMainLooper())
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        handler.postDelayed({
+            val intent = Intent(applicationContext, MainActivity::class.java)
+            startActivity(intent)
+        }, 200L)
+    }
+}
