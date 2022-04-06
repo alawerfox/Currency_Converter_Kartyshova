b396a889c70ea571c85668f106518b8b45d61f27
Картышова Анастасия <alawerfox@gmail.com>
2022-04-06 14:26:56 +0300

    Feature/81106 сurrency details

--- app/src/main/java/ru/pdr/currencyconverterkartyshova/CurrencyTransferViewModel.kt
+++ app/src/main/java/ru/pdr/currencyconverterkartyshova/CurrencyTransferViewModel.kt
@@ -1,7 +1,30 @@
 package ru.pdr.currencyconverterkartyshova
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class CurrencyTransferViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    private val _rub = MutableLiveData<Double>()
+    val rub: LiveData<Double>
+        get() = _rub
+
+    private val _currency = MutableLiveData<Double>()
+    val currency: LiveData<Double>
+        get() = _currency
+
+    private var amount: Double = 0.0
+
+    fun setAmount(amount: Double) {
+        this.amount = amount
+    }
+
+    fun exchangeToRub(value: Double) {
+        _rub.value = value * amount
+    }
+
+    fun exchangeToCurrency(value: Double) {
+        _currency.value = if(value == 0.0) 0.0 else value/amount
+    }
+
 }
